e04370dad5bceafa1f7d528a919000f34942cd5c
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-03-27 08:40:34 +0100

    Added SASS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Mathias Soeholm | Fullstack Mobile Developer`,
+    description: `The online portfolio of a fullstack mobile developer, with an expertise in creating responsive and polished user layouts.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
