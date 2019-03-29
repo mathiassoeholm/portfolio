98601d4ef75ca1305b41cbb5f652eca89129200e
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-03-29 21:35:38 +0100

    Did some work on line animation

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -1,11 +1,12 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
 import Line from "./line"
 
 const LandingPage: React.FC = () => {
+
   return (
-    <div className={'pf-landing-page'}>
-      <Line left={'10%'} height={'50%'} />
-      <Line left={'60%'} height={'20%'} />
+    <div className={'pf-landing-page'}  >
+      <Line left={'10%'} heightInRem={50} />
+      <Line left={'60%'} heightInRem={20} />
     </div>
   )
 }
--- src/components/line.tsx
+++ src/components/line.tsx
@@ -1,14 +1,87 @@
-import React from 'react';
+import React, { Component, useEffect, useState } from "react"
+import posed from "react-pose"
+import { CurrentPose } from "react-pose/lib/components/PoseElement/types"
 
 interface Props {
   left: string,
-  height: string,
+  heightInRem: number,
 }
 
-const Line: React.FC<Props> = (props: Props) => (
-  <div className={'pf-line'} style={{ left: props.left, height: props.height }}>
-    <div className={'pf-line-head'} />
-  </div>
-);
+interface State {
+  pose: string,
+}
+
+class Line extends Component<Props, State> {
+  state: State = {
+    pose: 'hidden',
+  };
+
+  PosedLine = posed.div({
+    hidden: { height: '0%' },
+    visible: {
+      height: `${this.props.heightInRem}%`,
+    },
+    short: {
+      height: `${this.props.heightInRem}%`,
+      transition: {
+        duration: 2000,
+      },
+    },
+    long: {
+      height: `${this.props.heightInRem+1}%`,
+      transition: {
+        duration: 2000,
+      },
+    }
+  });
+
+  PosedLineHead = posed.div({
+    passive: {
+      // Set opacity based on parents height
+      opacity: [
+        'height',
+        (h: string) => h === '0%' ? 0 : 1,
+        true, // Listen to parent
+      ]
+    },
+  });
+
+  constructor(props) {
+    super(props);
+
+    this.onPoseComplete = this.onPoseComplete.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ pose: 'visible' });
+  }
+
+  onPoseComplete(currentPose: CurrentPose) {
+    switch (currentPose) {
+      case 'visible':
+        this.setState({ pose: 'long' })
+        break;
+      case 'short':
+        this.setState({ pose: 'long' })
+        break;
+      case 'long':
+        this.setState({ pose: 'short' })
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <this.PosedLine
+        className={'pf-line'}
+        style={{ left: this.props.left }}
+        pose={this.state.pose}
+        onPoseComplete={this.onPoseComplete}
+      >
+        <this.PosedLineHead className={'pf-line-head'}/>
+      </this.PosedLine>
+    )
+  }
+}
 
 export default Line;
