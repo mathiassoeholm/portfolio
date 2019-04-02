7f89e268ae5bd942cae9015568cab85150611adc
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-04-02 21:41:43 +0200

    Hooked up icons

--- src/components/icon-section.tsx
+++ src/components/icon-section.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import GithubIcon from '../assets/icons/github.svg'
+import LinkedInIcon from '../assets/icons/linkedin.svg'
+import EnvelopeIcon from '../assets/icons/envelope.svg'
 
 const IconSection : React.FC = () => {
   return (
     <div className={'pf-icon-section'}>
-      <GithubIcon className={'pf-icon-section-child'}/>
-      <GithubIcon className={'pf-icon-section-child'}/>
-      <GithubIcon className={'pf-icon-section-child'}/>
+      <a href={'https://www.linkedin.com/in/mathias-soeholm-1887a438/'}>
+        <LinkedInIcon className={'pf-icon-section-child'}/>
+      </a>
+      <a href={'https://github.com/mathiassoeholm'}>
+        <GithubIcon className={'pf-icon-section-child'}/>
+      </a>
+      <a href={'mailto:mathiassoeholm@gmail.com'}>
+        <EnvelopeIcon className={'pf-icon-section-child'}/>
+      </a>
     </div>
   );
 }
