ab86e264c6cb5210f4a7d2c1078b83719fcb7f08
Mathias <mathiassoeholm@gmail.com>
2019-04-02 11:20:46 +0200

    Should not see font blink anymore

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 
 import LandingPage from "../components/landing-page"
 import Section2 from "../components/section-2"
 import SEO from "../components/seo"
 import { Helmet } from "react-helmet"
 
-const IndexPage = () => (
-  <div>
-    <Helmet>
-      <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet" />
-    </Helmet>
-    <SEO title={'Landing Page'} />
-    <LandingPage />
-    <Section2 />
-  </div>
-);
+const IndexPage = () => {
+  const canCheckFonts = document.fonts !== undefined;
+  const [didLoadFonts, setDidLoadFonts] = useState(false);
+
+  if (!didLoadFonts && canCheckFonts) {
+    document.fonts.ready.then( () => {
+      setDidLoadFonts(true);
+    });
+
+    return <div className={'pf-landing-page'} />;
+  } else {
+    return (
+      <div>
+        <Helmet>
+          <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet" />
+        </Helmet>
+        <SEO title={'Landing Page'} />
+        <LandingPage />
+        <Section2 />
+      </div>
+    );
+  }
+}
 
 export default IndexPage
