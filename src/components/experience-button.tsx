c6179642647bfa80d6d57f8b3fe2655e73c72bdb
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-04-04 19:38:33 +0200

    Created animation for experience section

--- src/components/experience-button.tsx
+++ src/components/experience-button.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import posed from "react-pose"
 
 interface Props {
   text: string,
+  onClick: () => void,
+  selected?: boolean | undefined,
 }
+
+const PosedButton = posed.button({
+  selected: {
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    paddingLeft: '1.3em',
+  },
+  unselected: {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    paddingLeft: '0.2em',
+  }
+});
 
 const ExperienceButton: React.FC<Props> = (props: Props) => {
   return (
-    <div className={'pf-experience-button'}>
+    <PosedButton
+        className={'pf-experience-button'}
+        onClick={props.onClick}
+        pose={props.selected ? 'selected' : 'unselected'}
+    >
       {props.text}
-    </div>
+    </PosedButton>
   );
 }
 
--- src/components/experience-section.tsx
+++ src/components/experience-section.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from "react"
 import SectionHeader, { SectionHeaderType } from "./section-header"
 import ExperienceButton from "./experience-button"
+import posed from "react-pose"
+
+const numberOfButtons = 6;
+
+const PosedDiv = posed.div({
+  _1: { top: `${0/numberOfButtons*100}%`},
+  _2: { top: `${1/numberOfButtons*100}%`},
+  _3: { top: `${2/numberOfButtons*100}%`},
+  _4: { top: `${3/numberOfButtons*100}%`},
+  _5: { top: `${4/numberOfButtons*100}%`},
+  _6: { top: `${5/numberOfButtons*100}%`},
+});
 
 const ExperienceSection: React.FC = () => {
+
+  const [pose, setPose] = useState('_1');
+
+  const createExperienceButton = (text: string, index: number) => (
+    <ExperienceButton
+      key={index}
+      text={text}
+      onClick={() => setPose(`_${index+1}`)}
+      selected={pose === `_${index+1}`}
+    />
+  );
+
   return (
     <div className={'pf-experience-section'}>
       <SectionHeader type={SectionHeaderType.Experience} />
       <div className={'pf-experience-container'}>
+        <div className={'pf-experience-left-border-line'}/>
         <div className={'pf-experience-button-column'}>
-          <ExperienceButton text={'Mindfox Studios'} />
-          <ExperienceButton text={'LEGO'} />
-          <ExperienceButton text={'BitBreak'} />
-          <ExperienceButton text={'Kiloo'} />
-          <ExperienceButton text={'Campus Djursland'} />
-          <ExperienceButton text={'ClearCut Games'} />
-          <div className={'pf-experience-marker'}/>
+          {
+            [
+              'Mindfox',
+              'LEGO',
+              'BitBreak',
+              'Kiloo',
+              'Campus Dj.',
+              'ClearCut',
+            ].map(createExperienceButton)
+          }
+          <PosedDiv className={'pf-experience-marker'} pose={pose}/>
         </div>
         <div className={'pf-experience-text'}/>
       </div>
