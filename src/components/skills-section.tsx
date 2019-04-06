a479e25973903e7b7e6542e1e3b89420500803e2
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-04-06 18:26:52 +0200

    Added correct skill values

--- src/components/skills-section.tsx
+++ src/components/skills-section.tsx
@@ -11,6 +11,11 @@ const PosedDiv = posed.div({
   }
 });
 
+interface SkillDescription {
+  name: string,
+  value: number
+}
+
 let animationStarters: (() => void)[] = [];
 
 const SkillsSection: React.FC = () => {
@@ -32,16 +37,40 @@ const SkillsSection: React.FC = () => {
     }
   }
 
+  const renderSkillMeter = ({ name, value }: SkillDescription, index: number) => {
+    return (
+      <SkillMeter
+        key={index}
+        skill={name}
+        value={value}
+        setStartAnimation={childFunc => animationStarters.push(childFunc)}
+      />
+    )
+  }
+
   return (
     <div className={'pf-skills-section'} >
       <SectionHeader type={SectionHeaderType.Skills}/>
       <div className={'pf-skill-container-parent'}>
         <VisibilitySensor onChange={startAnimation} partialVisibility>
           <PosedDiv className={'pf-skill-container'} pose={pose}>
-            <SkillMeter skill={'React'} value={0.9} setStartAnimation={childFunc => animationStarters.push(childFunc)} />
-            <SkillMeter skill={'TypeScript'} value={0.3} setStartAnimation={childFunc => animationStarters.push(childFunc)} />
-            <SkillMeter skill={'React Native'} value={0.6} setStartAnimation={childFunc => animationStarters.push(childFunc)} />
-            <SkillMeter skill={'React Native'} value={1.0} setStartAnimation={childFunc => animationStarters.push(childFunc)} />
+            {
+              [
+                {name: 'Unity', value: 0.95},
+                {name: 'React', value: 0.8},
+                {name: 'JavaScript (ES6)', value: 0.8},
+                {name: 'Flutter', value: 0.75},
+                {name: 'React Native', value: 0.7},
+                {name: 'C#', value: 0.7},
+                {name: 'TypeScript', value: 0.65},
+                {name: 'Swift', value: 0.65},
+                {name: 'Kotlin', value: 0.6},
+                {name: 'Java', value: 0.6},
+                {name: 'Sass', value: 0.5},
+                {name: 'Figma', value: 0.4},
+                {name: 'Photoshop', value: 0.3},
+              ].map(renderSkillMeter)
+            }
           </PosedDiv>
         </VisibilitySensor>
       </div>
