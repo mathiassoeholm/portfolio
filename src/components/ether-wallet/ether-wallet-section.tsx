a96dcfc57863f65dfb2ebdb1fc113032a5fafd04
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-04-09 17:26:04 +0200

    Started working on Grid Layou for ether wallet

--- src/components/ether-wallet/ether-wallet-section.tsx
+++ src/components/ether-wallet/ether-wallet-section.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ClippedSectionPart from "../clipped-section-part"
+import { Grid } from "@material-ui/core"
 
 const EtherWalletSection: React.FC = () => {
   return (
     <div>
       <ClippedSectionPart topLeft={8} className={'pf-ether-wallet-clip-part'}/>
       <div className={'pf-ether-wallet-background'}>
-        <div style={{height: '300px'}}></div>
+        <Grid container className={'pf-ether-wallet-content'}>
+          <Grid item xs={6} md={12}>
+            Ether Icon goes here
+          </Grid>
+          <Grid item xs={6} md={12}>
+            Title goes here
+          </Grid>
+          <Grid item xs={6} md={12}>
+            Description goes here
+          </Grid>
+          <Grid item xs={6} md={12}>
+            Screenshot goes here
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
