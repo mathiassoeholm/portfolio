a988062ddb25286aba03612f289853af0ef55e6a
Mathias <mathiassoeholm@gmail.com>
2019-04-10 12:19:31 +0200

    Added links

--- src/components/ether-wallet/ether-wallet-description.tsx
+++ src/components/ether-wallet/ether-wallet-description.tsx
@@ -14,11 +14,10 @@ const EtherWalletDescription: React.FC = () => {
         Technology
       </h2>
       <p>
-        The front-end was developed in React using the Material-UI framework.
-        The backend was built with Node.JS and Firebase Cloud Firestore for storing data.
-        We used Redux for state management and Redux Saga for handling side effects.
-        Test-Driven development (TDD) reassured us that the code works and enabled refactoring which resulted in a clean code-base.
-        Jest was used as testing framework.
+        The front-end was developed in <a href={'https://reactjs.org/'} target={'_blank'}>React</a> using the <a href={'https://material-ui.com/'} target={'_blank'}>Material-UI</a> framework.
+        The backend was built with <a href={'https://nodejs.org'} target={'_blank'}>Node.JS</a> and <a href={'https://firebase.google.com/products/storage'}>Firebase Cloud Firestore</a> for storing
+        data. <a href={'https://redux.js.org'} target={'_blank'}>Redux</a> was used for state management and <a href={'https://redux-saga.js.org/'} target={'_blank'}>Redux Saga</a> for handling side effects.
+        Test-Driven development (TDD) reassured that the code works and enabled refactoring which resulted in a clean code-base. <a href={'https://jestjs.io'} target={'_blank'}>Jest</a> was used as testing framework.
       </p>
     </div>
   );
