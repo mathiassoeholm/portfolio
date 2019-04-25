d45ccabd6bb4ce4448a64b12a1a5336410f13c79
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-04-25 10:15:54 +0200

    Set up testing infrastructure

---
+++ __mocks__/file-mock.js
@@ -0,0 +1,5 @@
+/*
+ * This mock will be used when assets are imported in tests.
+ * See jest.config.js
+ */
+module.exports = "test-file-stub"
---
+++ src/components/experience-section/experience-description-text.test.ts
@@ -0,0 +1,3 @@
+it('passes the test', () => {
+  expect(2+2).toBe(4)
+})
